cba103e8bc5da2c0301edc067ba5b24621ae5ca0
nasirhnagri512 <hussainnasir8755@gmail.com>
2024-05-22 22:28:00 +0500

    some chaneges

--- app.js
+++ app.js
@@ -7,3 +7,31 @@ let form = document.querySelector("form")
 const proPagation = (e) => {
     e.stopPropagation()
 }
+
+const signUpData = [
+    {   
+         id:1,
+        FullName: "muzamil hussain",
+        email: "meixuHussain.007@gmail.com",
+        password: "12345",
+        confirmPassword: "12345",
+    }, {
+        id:2,
+        FullName: "imran ali",
+        email: "a@gmail.com",
+        password: "abcd",
+        confirmPassword: "abcd"
+    }, {
+        id:3,
+        FullName: "jabir jabir",
+        email: " ",
+        password: "zero1122",
+        confirmPassword: "zero1122"
+    },
+    newSignUp={
+        FirstName:"",
+        Email:"",
+        passPassword:"",
+        passConfirmPassword:""
+    }
+]
