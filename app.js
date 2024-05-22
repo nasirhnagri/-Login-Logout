cf0361a87b2e05e450e1473d5d7f2a3ec010cddc
nasirhnagri512 <hussainnasir8755@gmail.com>
2024-05-22 22:25:53 +0500

    js file

---
+++ app.js
@@ -0,0 +1,9 @@
+
+let logIn = document.getElementById("logInBtn")
+let body = document.querySelector("body")
+let signUp = document.getElementById("signUp")
+let offForm = document.getElementById("user")
+let form = document.querySelector("form")
+const proPagation = (e) => {
+    e.stopPropagation()
+}
