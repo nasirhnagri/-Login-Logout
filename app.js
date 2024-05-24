c79b0b971d51afa89438a083ac4aabc1dac7640e
nasirhnagri512 <hussainnasir8755@gmail.com>
2024-05-24 23:08:08 +0500

    "Add some javascript"

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 let logIn = document.getElementById("logInBtn")
 let body = document.querySelector("body")
 let signUp = document.getElementById("signUp")
@@ -35,3 +34,54 @@ const signUpData = [
         passConfirmPassword:""
     }
 ]
+const render = () => {
+    let email = document.getElementById("mail").value
+    let password = document.getElementById("pass").value
+    // console.log(email,password)
+
+    let authentication = signUpData?.filter((item) => {
+        if(item.email === email && item.password === password){
+            return item
+        }
+    }).length;
+    console.log(authentication)
+    let newPage = "";
+    let page = document.getElementById("page-two")
+    if (authentication) {
+        signUpData.map((item) => {
+            newPage += `
+            <table>
+            <div id="secretData">
+            <tr class="flex"> 
+            <th>Email</th>
+            <td class="Data">E-mail::${item.email}</td> 
+            <th>password</th>
+            <td class="Data">Password::${item.password}</td>
+            </tr>
+            <div class="btns">
+            <button onclick="openDaletModel(${item.id})" id="delet">Delet<button>
+            <button onclick="edit(${item.id})" id="edit">Edit<button>
+            <button onclick="Veiw(${item.id})" id="forest">View<button>
+            </div>
+            </div>
+            </table>
+            `
+        })
+        page.innerHTML = newPage
+        page.style.display = "flex"
+    } else {
+        let error = document.getElementById("Error")
+        let errorOne = "";
+        errorOne = `
+     <h5 class= "incorrect">E-mail and password is Incorrect</h5>
+     `
+        error.innerHTML = errorOne;
+        error.style.display = "flex"
+        if (errorOne) {
+            setTimeout(() => {
+                error.style.display = "none"
+            }, 3000);
+        }
+    }
+    authentication ? offForm.style.display = "none" : offForm.style.display = "block";
+}
